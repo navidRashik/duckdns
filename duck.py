df28b537ea3b11947ea28397c3e69e4069760c88
navid.rashik@gmail.com <navid.rashik@gmail.com>
2022-05-02 12:15:58 +0600

    updated py script

--- duck.py
+++ duck.py
@@ -1,14 +1,16 @@
+import imp
 import netifaces
 import requests
 from os import environ
 from dotenv import load_dotenv
-
+from pprint import pprint
+from datetime import datetime
 load_dotenv()
 
-
-
-netifaces.interfaces()
+print("-----------------Starts---------------------",datetime.now())
+pprint(netifaces.interfaces())
 addrs = netifaces.ifaddresses("wlp3s0")
+pprint(addrs)
 ipv6 = addrs[netifaces.AF_INET6][0]["addr"]
 print(ipv6)
 response = requests.get(
@@ -21,4 +23,11 @@ response = requests.get(
 )
 
 # response = requests.get(url="https://www.duckdns.org/update?domains=navidrashik&token=bb26ad54-9c69-41d7-b524-908ca10e515e&ip=")
-print(response.status_code, response.text,response.url,environ.get("DOMAINS"))
+pprint(
+    {
+        "status_code": response.status_code,
+        "response_txt": response.text,
+        "url": response.url,
+        "doman_name": environ.get("DOMAINS"),
+    }
+)
--- duck.sh
+++ duck.sh
@@ -1,4 +1,4 @@
 
 # echo url="https://www.duckdns.org/update?domains=navidrashik&token=bb26ad54-9c69-41d7-b524-908ca10e515e&ip=" | curl -k -o ~/duckdns/duck.log -K -
 sudo apt install python3-pip
-pip install requests netifaces python-dotenv && python3 duck.py > duck.log 
+pip install requests netifaces python-dotenv && python3 duck.py >> duck.log 
